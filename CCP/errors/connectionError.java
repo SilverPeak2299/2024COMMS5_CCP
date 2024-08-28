6c5bfea7a49c647f6a3d129f8e7bde0ee095994a
Latthasay Saisanavong <Latthasay@gmail.com>
2024-08-28 14:07:01 +1000

    start ccp program

---
+++ CCP/errors/connectionError.java
@@ -0,0 +1,8 @@
+package CCP.errors;
+
+public class connectionError extends Exception{
+
+    public connectionError(String message) {
+        super(message);
+    }
+}
---
+++ CCP/espConnection.java
@@ -0,0 +1,28 @@
+package CCP;
+import java.io.*;
+import java.net.*;
+
+
+public class espConnection extends Connection {
+    private DataGram
+
+    espConnection(int id) {
+        super(id);
+        //TODO Auto-generated constructor stub
+    }
+
+
+    @Override
+    public boolean inialiseConnection() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+
+    @Override
+    public boolean isConnected() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+    
+}
